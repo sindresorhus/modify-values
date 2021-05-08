54d25c66e5a8e9f1315c6dc8ae8dc7206fcc4c98
Richie Bendall <richiebendall@gmail.com>
2021-05-09 05:05:19 +1200

    Add TypeScript definition (#3)
    Co-authored-by: Sindre Sorhus <sindresorhus@gmail.com>

---
+++ index.d.ts
@@ -0,0 +1,14 @@
+/**
+Modify the values of an object.
+
+@param transformer - Gets the value and key for each item and is expected to return the new value.
+
+@example
+```
+import modifyValues from 'modify-values';
+
+modifyValues({foo: 'UNICORN'}, (value, key) => value.toLowerCase());
+//=> {foo: 'unicorn'}
+```
+*/
+export default function modifyValues<KeyType extends string | number | symbol, ValueType, ReturnValueType>(object: Record<KeyType, ValueType>, transformer: (key: KeyType, value: ValueType) => ReturnValueType): Record<KeyType, ReturnValueType>;
